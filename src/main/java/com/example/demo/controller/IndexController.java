8cffe1204bcf71f51c950a713089baf015f3e589
xhb233 <1203476754@qq.com>
2020-06-10 18:22:30 +0800

    2020-6-10

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class helloController {
+public class IndexController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-        return "hello";
+        return "index";
     }
 }
