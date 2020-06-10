98874352cc79ce8a87f9a68fe21ebdd54239fbf9
xhb233 <1203476754@qq.com>
2020-06-10 22:49:12 +0800

    2020-6-10

---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AccessTokenDTO;
+import com.example.demo.dto.GithubUser;
+import com.example.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author XieHaiBin
+ * @Date 2020/6/10 18:40
+ * @Version 1.0
+ */
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("9407f4127e0d14d33e21");
+        accessTokenDTO.setClient_secret("08ecf48a335418c5689085d4f58f6dacbe11d291");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/demo/dto/GithubUser.java
@@ -0,0 +1,36 @@
+package com.example.demo.dto;
+
+/**
+ * @Author XieHaiBin
+ * @Date 2020/6/10 21:40
+ * @Version 1.0
+ */
+public class GithubUser {
+    private String name;
+    private long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
